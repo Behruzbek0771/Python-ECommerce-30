389ba44745cfd09e868c71fa81339b048ebdfc3a
Behruzbek0771 <behruzbekqalandarov771@gmail.com>
2025-07-25 10:12:03 +0500

    Bajarildi

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
+import json
+from sys import exit
+from models.product import Product
 from models.user import User
+from utils.printers import print_menu
 
+while True:
+    print_menu()
+    
+    tanlov = int(input("tanlang : "))
+    if tanlov == 1:
+        User.create_user()
 
-User.create_user()
+    elif tanlov == 2:
+        products = Product.loads_product()
+
+        for product in products:
+            print(product)
+            
+        print("Sotib olmoqchi bo'lgan mahsulotingiz narxi qanchadan yuqori bo'lishi kerak! \n")
+        Product.filter_price()
+
+        product_tanlang = int(input("Sotib olmoqchi bo'lgan mahsulotingiz ID sini kiriting: \n"))
+
+        for product in products:
+            if product.id == product_tanlang:
+                if product.quantity > 0:
+                    product.quantity -= 1
+                else:
+                    print("Bu mahsulot tugagan!")
+
+        product_dicts = [p.to_dict() for p in products]
+
+        with open("database/products.json", 'w', encoding="utf-8") as jsonfile:
+            json.dump(product_dicts, jsonfile, ensure_ascii=False, indent=4)
+
+    elif tanlov == 0:
+        exit(0)
--- models/product.py
+++ models/product.py
@@ -1,7 +1,83 @@
 from datetime import datetime
+import json
 
 
 class Product:
     
-    def __init__(self,):
-        pass
+    def __init__(
+            self,
+            id,
+            name,
+            description,
+            price,
+            quantity,
+            category,
+            brand,
+            in_stock,
+            created_at,
+            updated_at,
+        ) -> None:
+        
+        self.id = id
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+        self.category = category
+        self.brand = brand
+        self.in_stock = in_stock
+        self.created_at = created_at
+        self.updated_at = updated_at
+        
+    def to_dict(self):
+        return {
+            "id":self.id,
+            "name":self.name,
+            "description":self.description,
+            "price":self.price,
+            "quantity":self.quantity,
+            "category":self.category,
+            "brand":self.brand,
+            "in_stock":self.in_stock,
+            "created_at":self.created_at.strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "updated_at":self.updated_at.strftime("%Y-%m-%dT%H:%M:%SZ"),
+        }
+        
+    @classmethod
+    def from_dict(cls, data:dict):
+        data = cls(
+            id = data['id'],
+            name = data['name'],
+            description = data['description'],
+            price = data['price'],
+            quantity = data['quantity'],
+            category = data['category'],
+            brand = data['brand'],
+            in_stock = data['in_stock'],
+            created_at = datetime.strptime(data['created_at'],"%Y-%m-%dT%H:%M:%SZ"),
+            updated_at =  datetime.strptime(data['updated_at'],"%Y-%m-%dT%H:%M:%SZ"),
+        )
+        
+        return data
+    
+    @classmethod
+    def loads_product(cls):
+        with open("database/products.json",'r',encoding="utf-8") as jsonfile:
+            data = json.load(jsonfile)
+            product = []
+            for item in data:
+                product.append(cls.from_dict(item))
+            
+            return product
+        
+    @classmethod
+    def filter_price(cls):
+        price = float(input("enter price : "))
+        products = cls.loads_product() 
+        for product in products:
+            if product.price > price:
+                print(product)
+                
+    
+    def __str__(self):
+        return f"{self.id}-id  {self.name}  {self.price} so'm"
